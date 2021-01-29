3cf4b1078f73f6637b472781a295a36e80f26394
Daniel Mathiot <d.danymat@gmail.com>
2021-01-29 09:04:29 +0100

    feat: Allowed link traversal

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-
 import re
 import os
 from sys import argv
@@ -14,10 +13,11 @@ class Parser:
         self._called = False
         self._retrieveMarkdownFiles()
         self._currentFileAsHtml = None
-        self._addedFiles = []
+        self._addedFiles = set()
 
     def _retrieveMarkdownFiles(self):
-        """
+        """Directory traversal to find all .md files and stores them in _mdFiles
+
         Full credit goes to: https://github.com/archelpeg
         """
         if self._called: raise Exception('Files have already been retrieved')
@@ -32,27 +32,32 @@ class Parser:
         self._called = True
 
     def _findFilesWithTag(self, tag=None):
-        filesWithTag = []
+        """Find all files containing a specific tag
+        """
         for file in self._mdFiles:
             self._openFileAsString(file)
             if self._tagInCurrentFile(tag):
-                filesWithTag.append(file)
-        return filesWithTag
+                self._addedFiles.add(file)
 
 
     def _tagInCurrentFile(self, tag):
+        """Check if tag is in Current File
+        """
         tags = set(re.findall(self._regexFindTags, self._currentFileAsHtml))
         tags = [tag[1:] for tag in tags]
         return tag in tags
 
     def _findLinksInCurrentFile(self):
+        """Return all links in the Current File"""
         return re.findall(self._regexFindLinks, self._currentFileAsHtml)
 
     def _openFileAsString(self, fileName):
+        """Open one file and store the content in _currentFileAsHtml"""
         with open(fileName, "r", encoding="utf-8") as input_file:
             self._currentFileAsHtml = input_file.read()
 
     def _exportInZip(self, files, path=None):
+        """Export all files in zip, if path not specified, export to current location"""
         if len(files) == 0:
             print('No files found, exiting now'); return
         if path == None:
@@ -61,28 +66,54 @@ class Parser:
         else:
             print(f'Exporting to {path}...')
         zipObj = ZipFile(f'Exported_{self._tag}.zip', 'w')
+        print('###### FILES FOUND ######')
         for file in files:
-            zipObj.write(file)
+            print(file)
+            fileName = file.rsplit(f'{self._folderPath}/', 1)[-1]
+            zipObj.write(file, os.path.join('.',fileName)) # Not sure if it recreates subfolders
         zipObj.close()
+        print('######################')
+        print('Exported!')
 
-    def run(self):
-        files = self._findFilesWithTag(self._tag)
-        print(files)
-        self._exportInZip(files)
+    def _findSubFilesForAddedFiles(self):
+        """Iteration to grow _addedFiles while i can"""
+        while not self._grow():
+            pass
 
 
+    def _grow(self):
+        """Add new files found following links in _addedFiles"""
+        addedFiles = set()
+        for file in self._addedFiles:
+            self._openFileAsString(file)
+            linkedFiles = self._findLinksInCurrentFile() # all links in file
+            linkedFiles = [link+'.md' for link in linkedFiles] # Added .md at the end
+            linkedFiles = [file # Get the full links
+                for file in self._mdFiles
+                for link in linkedFiles
+                if link in file
+            ]
+            linkedFiles = set(linkedFiles) - self._addedFiles # Only keep not added files
+            for link in linkedFiles:
+                addedFiles.add(link)
+        for file in addedFiles:
+            self._addedFiles.add(file)
+        return len(addedFiles) == 0
+
+    def run(self, recursive=False):
+        """Run the script
+
+        * If recursive option, will find all linkedFiles in order to keep the clicks
+        """
+        self._findFilesWithTag(self._tag)
+        if recursive:
+            self._findSubFilesForAddedFiles()
+        self._exportInZip(self._addedFiles)
 
 
-if len(argv) != 4: raise Exception("Usage: python3 vault_folder --tag your_tag")
+if len(argv) not in [4,5]: raise Exception("Usage: python3 vault_folder --tag your_tag")
 folder = argv[1]
 tag = argv[3]
+
 parser = Parser(folderPath=folder, tag=tag)
-parser.run()
-
-
-
-
-
-
-
-
+parser.run(recursive='-r' in argv)
