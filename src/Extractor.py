d32dc87df2e63d1f24f0df5a2d796ff704c363a0
fietz <72164229+archelpeg@users.noreply.github.com>
2021-02-12 19:27:25 +0100

    made subtags work for extraction

--- src/Extractor.py
+++ src/Extractor.py
@@ -14,7 +14,14 @@ class Extractor:
         else:
             print(f'Exporting to {path}...')
 
-        zipObj = ZipFile(f'Exported_{zipName}.zip', 'w')
+        newFileName = ''
+        for character in zipName:
+            if character == '/':
+                newFileName += '-'
+            else:
+                newFileName += character
+
+        zipObj = ZipFile(f'Exported_{newFileName}.zip', 'w')
         print('###### FILES FOUND ######')
         for file in files:
             print(file.fileName)
